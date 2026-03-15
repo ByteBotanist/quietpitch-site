9769ff815aca5d6d53dd0a0452515e39bd997012
Caleb Meredith <thegolddolphin@Calebs-MBP.lan>
2026-03-15 01:42:32 -0400

    Add regulatory status to signup

--- components/EarlyAccessForm.tsx
+++ components/EarlyAccessForm.tsx
@@ -142,7 +142,7 @@ export default function EarlyAccessForm() {
         name="regulatoryStatus"
         required
         defaultValue=""
-        className="w-full rounded-m border border-white/20 bg-black/20 text-white px-4 py-3"
+        className="w-full rounded-xl border border-white/20 bg-black/20 px-5 py-4 text-white appearance-none"
       >
         <option value="">Select...</option>
         <option value="Not registered / Educational">Not registered / Educational</option>
